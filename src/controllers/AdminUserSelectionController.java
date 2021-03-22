e0ba10591cd9d2c521c66a9a9d2b12272301616e
Vladik Miruk <vladislav.lapimiruk@studio.unibo.it>
2021-03-22 18:41:28 +0100

    Aggiunta schermata iniziale e quella di login amministratore e controller non completo della selezione utente/amministratore

---
+++ src/controllers/AdminUserSelectionController.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import javafx.event.ActionEvent;  
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AdminUserSelectionController {
+	
+	@FXML 
+	public void AdvanceToLogin(ActionEvent event)  throws Exception {
+		
+		       Parent root = FXMLLoader.load(getClass().getResource("/layouts/AdminLogin.fxml"));
+		    
+		        Scene scene = new Scene(root, 600, 500);
+		        Stage stage = new Stage();
+		    
+		        stage.setTitle("Sedibus");
+		        stage.setScene(scene);
+		        stage.show();
+		    }
+	}
---
+++ src/view/LoaderAdminUserSelection.java
@@ -0,0 +1,26 @@
+package view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
+
+
+
+
+public class LoaderAdminUserSelection extends Application {
+	
+	
+	@Override
+    public void start(Stage stage) throws Exception {
+       Parent root = FXMLLoader.load(getClass().getResource("/layouts/AdminUserSelection.fxml"));
+    
+        Scene scene = new Scene(root, 600, 500);
+    
+        stage.setTitle("Sedibus");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
