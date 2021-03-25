07877e0f1d4792be46a9a067f9bdf800f74f5e10
nicola costa <nicola.costa5@studio.unibo.it>
2021-03-25 21:57:16 +0100

    Creato il modello per la prenotazione con alcuni metodi. Istanziato poi nel controller.

--- src/controllers/ControllerPrenotazione.java
+++ src/controllers/ControllerPrenotazione.java
@@ -1,18 +1,39 @@
 package controllers;
 
+import model.ModelPrenotazione;
 import view.ViewPrenotazione;
 
 public class ControllerPrenotazione {
 	
 	private ViewPrenotazione vista;
+	private ModelPrenotazione modello;
 	
-	
-	public ControllerPrenotazione(ViewPrenotazione vista) {
+	public ControllerPrenotazione(ViewPrenotazione vista, ModelPrenotazione modello) {
 		this.vista = vista;
+		this.modello = modello;
 	}
 	
 	public void assegnaGestori() {
+		this.vista.getBottonePiuPosti().setOnAction(e -> {
+			this.modello.aggiungiPosto();
+			//this.vista.setCampoPosti(Integer.toString(this.modello.getNumeroPosti()));
+		});
 		
+		this.vista.getBottoneMenoPosti().setOnAction(e -> {
+			this.modello.togliPosto();
+		});
+		
+		this.vista.getBottonePrenota().setOnAction(e -> {
+			
+		});
+		
+		this.vista.getBottoneReset().setOnAction(e -> {
+			
+		});
+		
+		this.vista.getBottoneAnnulla().setOnAction(e -> {
+			
+		});
 	}
 	
 }
---
+++ src/model/ModelPrenotazione.java
@@ -0,0 +1,35 @@
+package model;
+
+import java.util.Random;
+
+public class ModelPrenotazione {
+	
+	private int nPosti;
+	private Random rand = new Random();
+	private int numeroCod = 0;
+	private final String lettereCod = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	
+	public ModelPrenotazione() {
+		this.nPosti = 0;
+	}
+	
+	public int getNumeroPosti() {
+		return this.nPosti;
+	}
+	
+	public void aggiungiPosto() {
+		this.nPosti++; //il massimo per ora non l'ho messo
+	}
+	
+	public void togliPosto() {
+		if(this.nPosti != 0) {
+			this.nPosti--;
+		}
+	}
+
+	private String generaCodice() {
+		this.numeroCod++;
+		return this.lettereCod.charAt(this.rand.nextInt(this.lettereCod.length())) + String.valueOf(this.numeroCod);
+	}
+	
+}
