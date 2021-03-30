7ef865c5a3f2404a92ed58bc198c7da6898625a4
Rostyslav <rostyslav.dovganyuk@studio.unibo.it>
2021-03-30 20:02:03 +0200

    Raggruppato tutto in un unico fxml file per comodita e semplicita

--- src/view/LoaderTableView.java
+++ src/view/LoaderTableView.java
@@ -6,10 +6,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-import controllers.DatePickerController;
-import controllers.MainTableController;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import model.MainTableModel;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -21,24 +20,33 @@ public class LoaderTableView extends Application {
 	
 	private static final int PROPORTION = 2;
 
+	/*
 	public static void main(String[] args) {
 		launch(args);
 	}
+	*/
 	
 	
 	
-	private static String TAVOLO_VERDE_STYLE_PATH = "layouts/tavoloLibero.css";
-	private static String TAVOLO_ROSSO_STYLE_PATH = "layouts/tavoloOccupato.css";
 	
-	private List<Button> listaBottoni = new ArrayList<>();
-	private MainTableController controllerTable = new MainTableController();
+	/*
+	public LoaderTableView(List<Integer> idPrenotati, List<Integer> idTavoli) {
+		this.idTavoliRossi = controller.idTavoliPrenotati();
+		this.idTavoli = controller.idTavoliTotale();
+		System.out.println("[LoaderTableView] Tavoli totali: " + this.idTavoli + " Tavoli Rossi:" + this.idTavoliRossi);
+	}
+	*/
+	
+	public void loadView() {
+		launch();
+	}
 	
 	
 	@Override
 	public void start(Stage primaryStage) {
 		
 		try {
-            final FXMLLoader loader = new FXMLLoader(getClass().getResource("/layouts/Sample.fxml"));
+            final FXMLLoader loader = new FXMLLoader(getClass().getResource("/layouts/MappaTavoli.fxml"));
             //final FXMLLoader loader = new FXMLLoader(ClassLoader.getSystemResource("layouts/prova.fxml"));
             //tavoliView.start(primaryStage);
             
@@ -47,36 +55,20 @@ public class LoaderTableView extends Application {
             final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
             final int sw = (int) screen.getWidth();
             final int sh = (int) screen.getHeight();
-            final Scene scene = new Scene(root, 800, 500);
+            final Scene scene = new Scene(root);
             
-            //coloro i tavoli
-            colorTable(scene);
            
             //final Scene scene = new Scene(root, sw / PROPORTION, sh / PROPORTION);
             primaryStage.setTitle("Table Top View");
 	        primaryStage.setScene(scene);
 	        primaryStage.show();
-          
-
+	        
+           
         } catch (Exception e) {
             e.printStackTrace();
         }
 		
 		
-	}
-	
-	
-	private void colorTable(Scene scene) {
-		controllerTable.idTavoliTotale().forEach(e -> {
-			Button b = (Button)scene.lookup("#"+e); 
-			if(this.controllerTable.idTavoliPrenotati().contains(e)) {
-				b.getStylesheets().add(TAVOLO_ROSSO_STYLE_PATH);
-			}else {
-				b.getStylesheets().add(TAVOLO_VERDE_STYLE_PATH);	
-			}
-			
-			
-		});
 	}
 	
 	
