d44e767f384462be61ac066600e528ce57c59782
Vladik Miruk <vladislav.lapimiruk@studio.unibo.it>
2021-04-01 19:22:51 +0200

    Prima implementazione del login funzionante senza lettura da database

--- src/controllers/AdminLoginController.java
+++ src/controllers/AdminLoginController.java
@@ -7,34 +7,61 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+
 
 
 public class AdminLoginController {
 	
+	
 	private static final int PROPORTION = 2;
 	
 	@FXML
-	public void loadTableView(ActionEvent event) {  
-	 //Copia incolla del metodo dento view.LoaderTableView con alcune modifiche 
-		try {
-            final FXMLLoader loader = new FXMLLoader(getClass().getResource("/layouts/Sample.fxml"));
-            Stage primaryStage = new Stage();
-            //final FXMLLoader loader = new FXMLLoader(ClassLoader.getSystemResource("layouts/prova.fxml"));
-            
-            final Parent root = loader.load();
-            final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
-            final int sw = (int) screen.getWidth();
-            final int sh = (int) screen.getHeight();
-            final Scene scene = new Scene(root, sw / PROPORTION, sh / PROPORTION);
-            primaryStage.setTitle("Table Top View");
-	        primaryStage.setScene(scene);
-	        primaryStage.show();
-          
+	private TextField user;
+	
+	@FXML 
+	private PasswordField pass;
+	
+	@FXML
+	private Label failedLabel;
+	
+	
+	
+	 @FXML
+	 public void loadTableView(ActionEvent event) { 
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+	if (pass.getText().equals("pass") && user.getText().equals("utente"))
+	{	
+	  
+		 try {         //Copia incolla del metodo dento view.LoaderTableView con alcune modifiche 
+			 final FXMLLoader loader = new FXMLLoader(getClass().getResource("/layouts/MappaTavoli.fxml"));
+	            //final FXMLLoader loader = new FXMLLoader(ClassLoader.getSystemResource("layouts/prova.fxml"));
+	            //tavoliView.start(primaryStage);
+	            
+	            Stage primaryStage= new Stage();
+	            final Parent root = loader.load();
+	            final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+	            final int sw = (int) screen.getWidth();
+	            final int sh = (int) screen.getHeight();
+	            final Scene scene = new Scene(root);
+	            
+	           
+	            //final Scene scene = new Scene(root, sw / PROPORTION, sh / PROPORTION);
+	            primaryStage.setTitle("Table Top View");
+		        primaryStage.setScene(scene);
+		        primaryStage.show();
+		        
+	           
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+
 	}
-
+	else failedLabel.setText("Nome Utente o Password Errati");
+}
+    
 }
