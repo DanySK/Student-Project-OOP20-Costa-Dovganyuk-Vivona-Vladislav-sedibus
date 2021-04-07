36c1d25f7a66398a4c475cb3836e5be5403437ea
Rostyslav <rostyslav.dovganyuk@studio.unibo.it>
2021-04-07 22:42:10 +0200

    Collasso degli handler DatePicker e PeriodBox in un unico handler

--- src/model/ImplMainTableModel.java
+++ src/model/ImplMainTableModel.java
@@ -23,7 +23,6 @@ public class ImplMainTableModel implements MainTableModel {
 			listaID.add(t.getName());
 		});*/
 		ristorante.tavoliPrenotati(date, p).forEach(t ->{
-			System.out.println("ID tavolo: " + t.getName() );
 			listaID.add(t.getName());
 		});
 		
