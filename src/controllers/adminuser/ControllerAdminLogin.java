6dce4bdcb6891e5fb771fe3230db2fd0e1a502ad
Vladik Miruk <vladislav.lapimiruk@studio.unibo.it>
2021-04-19 20:47:44 +0200

    aggiunta del file json con i dati per il login e implementata la sua lettura nel controller AdminLogin

--- src/controllers/adminuser/ControllerAdminLogin.java
+++ src/controllers/adminuser/ControllerAdminLogin.java
@@ -2,36 +2,63 @@ package controllers.adminuser;
 
 
 
-import javafx.event.ActionEvent;  
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import view.piantina.LoaderTableView;
 import model.utili.Utente;
+import view.piantina.LoaderTableView;
 
 
 public class ControllerAdminLogin  {
-	
+	private static String LOGIN_FILE_PATH = System.getProperty("user.home") + System.getProperty("file.separator") +"logindata.json";
 	
 	@FXML
 	private TextField user;
-	
 	@FXML 
 	private PasswordField pass;
-	
 	@FXML
 	private Label failedLabel;
-	
 	private static Utente utente;
 	
-	;
+	private String fileUser;
+	private String filePassword;
+	/**
+	 * loginData imposta i campi fileUser e filePassword, che verrano usati per il login, leggendo il file logindata.json
+	 * 
+	 */
+	private void loginData () {
+		try{
+		Reader reader = new FileReader( LOGIN_FILE_PATH);//creo il file reader per il file logindata.json
+		JsonObject jobj = new Gson().fromJson(reader,JsonObject.class);//creo il JsonObject da cui andro a leggere i dati presenti su file 
+		this.fileUser=jobj.get("utente").getAsString();//uso il metodo getAsString invece di toString in modo che la stringa restituita non abbia i quote marks
+		this.filePassword= jobj.get("password").getAsString();
+	} catch (IOException e) {
+        e.printStackTrace();
 	
+	}
+	}
+
+/**
+ *  loadTableViewAdmin confronta il nome utente e password inseriti dall'utente con quelli presenti sul file logindata.json
+ *  nel caso di successo viene caricata la view dei tavoli passando come argomento l'Enum utente impostato su admin
+ */
 	 @FXML
 	 public void loadTableViewAdmin(ActionEvent event) {
+		 
+		 loginData();
 
-	  if (pass.getText().equals("pass") && user.getText().equals("utente"))
+	     
+	  if (pass.getText().equals(filePassword) && user.getText().equals(fileUser))//confronto i dati inseriti dall'utente con quelli sul file
 	  {	
 		utente=Utente.ADMIN;
 		
@@ -44,11 +71,12 @@ public class ControllerAdminLogin  {
 	     } catch (Exception e) {
 	            e.printStackTrace();
 	        }
-
+	
 	  }
 	  
 	  else failedLabel.setText("Nome Utente o Password Errati");
    }
+
+}	
     
 	
-}
