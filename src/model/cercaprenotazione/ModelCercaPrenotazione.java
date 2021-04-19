ea20b2fded43423ca924498294e29aea5d978921
nicola costa <nicola.costa5@studio.unibo.it>
2021-04-19 10:06:09 +0200

    Aggiustato il model e il controller di CercaPrenotazione

--- src/model/cercaprenotazione/ModelCercaPrenotazione.java
+++ src/model/cercaprenotazione/ModelCercaPrenotazione.java
@@ -1,15 +1,48 @@
 package model.cercaprenotazione;
 
+import java.time.LocalDate;
+
 import model.utili.Periodo;
 
 public interface ModelCercaPrenotazione {
 
 	/**
 	 * cerca i dati nel database
-	 * @param codice è il codice che il cliente ha inserito, quindi da cercare
-	 * @param cognome è il cognome che il cliente ha inserito, quindi da cercare
-	 * @return vero se li ha trovati, falso altrimenti
+	 * @param codice e' il codice inserito
+	 * @param cognome e' il cognome inserito
+	 * @param turno e' il turno scelto
+	 * @return vero se vengono trovati nel db, falso altrimenti
 	 */
-	boolean cercaDati(String codice, String cognome, Periodo turno);
+	boolean cercaDati(String cod, String cognome, Periodo turno);
+	
+	/**
+	 * @return il nome preso dalla prenotazione del cliente
+	 */
+	String getNome();
+	
+	/**
+	 * @return l'email presa dalla prenotazione del cliente
+	 */
+	String getEmail();
+	
+	/**
+	 * @return il telefono preso dalla prenotazione del cliente
+	 */
+	String getTelefono();
+	
+	/**
+	 * @return la data della prenotazione fatta dal cliente
+	 */
+	LocalDate getData();
+	
+	/**
+	 * @return il numero dei posti prenotati dal cliente
+	 */
+	String getPosti();
+	
+	/**
+	 * @return l'id/nome del tavolo occupato dal cliente
+	 */
+	String getIdTavolo();
 	
 }
--- src/model/cercaprenotazione/ModelCercaPrenotazioneImpl.java
+++ src/model/cercaprenotazione/ModelCercaPrenotazioneImpl.java
@@ -1,27 +1,66 @@
 package model.cercaprenotazione;
 
-import dataBaseModel.*;
+import java.time.LocalDate;
+
+import model.piantina.ImplRistorante;
+import model.piantina.Prenotazione;
+import model.piantina.Ristorante;
 import model.utili.Periodo;
 
 public class ModelCercaPrenotazioneImpl implements ModelCercaPrenotazione {
 
-	private GestoreDB db = new  ImplGestoreDB();
+	private Ristorante ristorante = new ImplRistorante();
 	private boolean prenotazTrovata;
+	private LocalDate data;
+	private Prenotazione prenotazione;
 	
 	public ModelCercaPrenotazioneImpl() {
 		this.prenotazTrovata = false;
 	}
-	
+
 	@Override
-	public boolean cercaDati(String codice, String cognome, Periodo turno) {
-		db.getMapPrenotazioni(turno).entrySet().forEach(elem -> {
+	public boolean cercaDati(String cod, String cognome, Periodo periodo) {
+		this.ristorante.getPrenotazioni(periodo).entrySet().forEach(elem -> {
 			elem.getValue().forEach(pren -> {
-				if(pren.getCodicePrenotazione().equals(codice) && pren.getCliente().getCognome().equals(cognome)) {
+				if (pren.getCodicePrenotazione().equals(cod) && 
+					pren.getCliente().getCognome().equals(cognome)) {
+					this.prenotazione = pren;
+					this.data = LocalDate.parse(elem.getKey());
 					this.prenotazTrovata = true;
 				}
 			});
 		});
 		return this.prenotazTrovata;
+	}
+	
+	@Override
+	public String getNome() {
+		return this.prenotazione.getCliente().getNome();
+	}
+	
+	@Override
+	public String getEmail() {
+		return this.prenotazione.getCliente().getEmail();
+	}
+	
+	@Override
+	public String getTelefono() {
+		return this.prenotazione.getCliente().getTelefono();
+	}
+	
+	@Override
+	public LocalDate getData() {
+		return this.data;
+	}
+	
+	@Override
+	public String getPosti() {
+		return String.valueOf(this.prenotazione.getPostiPrenotati());
+	}
+	
+	@Override
+	public String getIdTavolo() {
+		return String.valueOf(this.prenotazione.getTavolo().getName());
 	}
 
 }
